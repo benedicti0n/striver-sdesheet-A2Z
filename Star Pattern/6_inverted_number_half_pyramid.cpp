1c90fd2cc69087eaef0a8e3765915971ddbe69b0
benedicti0n <lbandopadhyay2@gmail.com>
2023-04-22 02:47:25 +0530

    star pattern upto 6 done on 22/04/23

---
+++ Star Pattern/6_inverted_number_half_pyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rows;
+    cout<<"Enter rows: ";
+    cin>>rows;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= (rows-i+1); j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;        
+    }
+    
+    
+    return 0;
+}
